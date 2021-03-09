d698a00c2fce59f962256fbac45ef4d6801ab3cb
Jade <applegreengrapeintherain@gmail.com>
2021-03-09 22:46:25 +0000

    simple tpl

---
+++ hey/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Index: React.FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+      <h1> Hello Next.js 👋 </h1>
+    </div>
+  )
+}
+
+export default Index
